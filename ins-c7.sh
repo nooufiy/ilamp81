0e644a32ba44d02c3bbbd00996acfc35d19eac60
noou <83199729+nooufiy@users.noreply.github.com>
2022-06-25 19:04:06 +0700

    Update ins-c7.sh

--- ins-c7.sh
+++ ins-c7.sh
@@ -61,4 +61,5 @@ mv /etc/logrotate.d/httpd /etc/logrotate.d/httpd.bak
 # nano /etc/logrotate.d/httpd
 sed -i '1 i\/home/l/*log' /etc/logrotate.d/httpd
 sed -i '1 i\minsize 1M' /etc/logrotate.d/httpd
+sed -i "s/\/var\/www\/html/\/home\/w/g" /etc/httpd/conf/httpd.conf
 systemctl restart httpd.service
